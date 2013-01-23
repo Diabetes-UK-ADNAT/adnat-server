beac60e29c24a22793fbe992723561e375ed9bd3
Eric Link <eric@ericmlink.com>
2013-01-22 22:43:19 -0800

    netbeans proj, staring with morphia dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,20 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+	//mongo
+	"com.google.code.morphia" % "morphia" % "0.99",
+	"org.mongodb" % "mongo-java-driver" % "2.7.3",
+	"com.google.code.morphia" % "morphia-logging-slf4j" % "0.99"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+	//mongo
+    resolvers += "Maven repository" at "http://morphia.googlecode.com/svn/mavenrepo/",
+	resolvers += "MongoDb Java Driver Repository" at "http://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/"
   )
+
+
+
 
 }
