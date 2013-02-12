842ba9e63dd58516254360b6110ae8ef7e614c67
Eric Link <eric@ericmlink.com>
2013-02-12 00:54:03 -0600

    api unit tests and controller updates from test results

--- app/controllers/FaqController.java
+++ app/controllers/FaqController.java
@@ -9,32 +9,42 @@ import play.mvc.BodyParser;
 import play.mvc.Result;
 
 public class FaqController extends BaseController {
-    
+
     @BodyParser.Of(value = BodyParser.Json.class)
     public static Result save() {
         Logger.debug("save");
         JsonNode json = getJsonFromBody();
         Faq faq = Json.fromJson(json, Faq.class);
         faq.id = getObjectId(json);
+        boolean create = faq.id == null;
         Faq.save(faq);
-        
-        // fixme return 201 created with location header or 200 ok for update
-        return okWithHeaders();
+        if (create) {
+            response().setHeader(LOCATION, "/v1/faqs/" + faq.id + ".json");
+            setHeaders();
+            return created();
+        } else {
+            return okWithHeaders();
+        }
     }
-    
+
     public static Result delete(String id) {
         Logger.debug(id);
         Faq.delete(id);
         return okWithHeaders();
     }
-    
+
     public static Result getAll() {
         Logger.debug("getAll");
         return okJsonWithHeaders(Faq.all());
     }
-    
+
     public static Result getById(String id) {
         Logger.debug(id);
-        return okJsonWithHeaders(Faq.find(id));
+        Faq faq = Faq.find(id);
+        if (faq != null) {
+            return okJsonWithHeaders(faq);
+        } else {
+            return okWithHeaders();
+        }
     }
 }
--- test/FaqControllerTest.java
+++ test/FaqControllerTest.java
@@ -4,86 +4,112 @@ import models.Faq;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.*;
-
 import play.mvc.*;
-import play.test.*;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 import play.libs.Json;
-import play.libs.WS;
-import static play.mvc.Controller.request;
 
 //import static org.fluentlenium.core.filter.FilterConstructor.*;
 //import static play.mvc.Controller.response;
 import static play.mvc.Http.Status.OK;
 
-//GET	/v1/faqs.json		controllers.FaqController.getAll()
-//GET	/v1/faqs/:id.json       controllers.FaqController.getById(id: String)
-//POST	/v1/faqs.json		controllers.FaqController.save()
-//#POST	/v1/faqs.json		controllers.FaqController.save2(faq: models.Faq)
-//DELETE	/v1/faqs/:id.json	controllers.FaqController.delete(id: String)
 public class FaqControllerTest {
 
-    Faq faq = new Faq();
+    String location = null;
 
     private void createItem() {
+        Faq faq = new Faq();
         faq.question = "question";
         faq.answer = "answer";
         faq.categories = new ArrayList();
         faq.categories.add("one");
         faq.categories.add("two");
         JsonNode json = Json.toJson(faq);
-        Result result =
-                routeAndCall(fakeRequest(POST, "/v1/faqs.json")
-                .withJsonBody(json));
-        assertThat(status(result)).isEqualTo(OK);
-        // location header
-        // 201 returned
+        Result result = routeAndCall(fakeRequest(POST, "/v1/faqs.json").withJsonBody(json));
+        assertThat(status(result)).isEqualTo(201);
+        location = header("Location", result);
+        assertThat(location).matches("^/v1/faqs/.*\\.json");
     }
 
-    private void getItem() {
-        // 200 
-        // all fields
-        // .equals orginal
+    private JsonNode getItem() {
+        try {
+            Result result = routeAndCall(fakeRequest(GET, location));
+            assertThat(status(result)).isEqualTo(OK);
+            ObjectMapper mapper = new ObjectMapper();
+            assertThat(contentAsString(result)).isNotNull();
+            return mapper.readTree(contentAsString(result));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    private void updateItem() {
-        // new values
-        // get(faq) 
+    private void updateItem(JsonNode json) { // fixme json serializing confusion
+        // update the item; change pojo and POST to location
+//        Result result =
+//                routeAndCall(fakeRequest(POST, location)
+//                .withJsonBody(json));
+        //assertThat(status(result)).isEqualTo(OK);
+        //assertThat(header("Location", result)).isEmpty();
     }
 
     private void deleteItem() {
-        // remove
-        // get returns empty not found
+        Result result = routeAndCall(fakeRequest(DELETE, location));
+        assertThat(status(result)).isEqualTo(OK);
     }
 
     @Test
-    public void faqControllerItemTest() {
+    public void itemCrud() {
         running(fakeApplication(), new Runnable() {
             public void run() {
                 createItem();
-                getItem();
-                updateItem();
+                updateItem(getItem());
                 deleteItem();
             }
         });
     }
 
-    private void createItems() {
-    }
-
-    private void getItems() {
+    @Test
+    public void list() {
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+                Result result;
+                createItem();
+                String loc1 = location;
+                createItem();
+                String loc2 = location;
+                createItem();
+                String loc3 = location;
+                //
+                result = routeAndCall(fakeRequest(GET, "/v1/faqs.json"));
+                assertThat(status(result)).isEqualTo(OK);
+                String list = contentAsString(result);
+                assertThat(list).isNotNull();
+                assertThat(list).isNotEmpty();
+                assertThat(list).matches("^\\[\\{.*id.*question.*answer.*id.*question.*answer.*");
+                //
+                result = routeAndCall(fakeRequest(DELETE, loc1));
+                assertThat(status(result)).isEqualTo(OK);
+                result = routeAndCall(fakeRequest(DELETE, loc2));
+                assertThat(status(result)).isEqualTo(OK);
+                result = routeAndCall(fakeRequest(DELETE, loc3));
+                assertThat(status(result)).isEqualTo(OK);
+            }
+        });
     }
 
     @Test
-    public void faqControllerListTest() {
+    public void itemNotFound() {
         running(fakeApplication(), new Runnable() {
             public void run() {
-                createItems();
-                getItems();
-//                deleteItems();
+                createItem();
+                //
+                Result result = routeAndCall(fakeRequest(GET, "/v1/faqs/5119e5293004dab11d02cc49.json"));
+                assertThat(status(result)).isEqualTo(OK);
+                String list = contentAsString(result);
+                assertThat(list).isEmpty();
+                //
+                deleteItem();
             }
         });
     }
