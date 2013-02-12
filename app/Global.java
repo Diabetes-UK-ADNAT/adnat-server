e68ffc62add3dc5c41a0d8993884d780f2130caf
Eric Link <eric@ericmlink.com>
2013-02-11 22:11:55 -0600

    prod.conf and start on tests

--- app/Global.java
+++ app/Global.java
@@ -5,26 +5,29 @@ import play.Logger;
 import com.google.code.morphia.Morphia;
 import com.mongodb.Mongo;
 import models.MorphiaObject;
+import play.Play;
 
 public class Global extends GlobalSettings {
 
-    @Override
-    public void onStart(play.Application arg0) {
-        super.beforeStart(arg0);
-        Logger.info("onStart");
+	@Override
+	public void onStart(play.Application arg0) {
+		super.beforeStart(arg0);
+		Logger.info("onStart");
 
-        try {
-            MorphiaObject.mongo = new Mongo("127.0.0.1", 27017);
-        } catch (UnknownHostException e) {
-            Logger.error("Could not connect to database", e);
-            Runtime.getRuntime().halt(-1);
-        }
+		try {
+			MorphiaObject.mongo = new Mongo("127.0.0.1", 27017);
+		} catch (UnknownHostException e) {
+			Logger.error("Could not connect to database", e);
+			Runtime.getRuntime().halt(-1);
+		}
 
-        MorphiaObject.morphia = new Morphia();
-        MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "adnat");
-        MorphiaObject.datastore.ensureIndexes();
-        MorphiaObject.datastore.ensureCaps();
+		MorphiaObject.morphia = new Morphia();
+		MorphiaObject.datastore = MorphiaObject.morphia.
+			createDatastore(MorphiaObject.mongo,
+			Play.application().configuration().getString("mongodb.name"));
+		MorphiaObject.datastore.ensureIndexes();
+		MorphiaObject.datastore.ensureCaps();
 
-        Logger.info("Morphia datastore: " + MorphiaObject.datastore.getDB());
-    }
+		Logger.info("Morphia datastore: " + MorphiaObject.datastore.getDB());
+	}
 }
