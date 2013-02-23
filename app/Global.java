324f6a9a0907bb04538468f8d5fc0d8e6b2eb777
Eric Link <eric@ericmlink.com>
2013-02-23 14:37:24 -0600

    play-authenticate working except for email verification flag (set manually restriction w/ deadbolt working)

--- app/Global.java
+++ app/Global.java
@@ -37,7 +37,7 @@ public class Global extends GlobalSettings {
         super.beforeStart(arg0);
         Logger.info("onStart");
         initMorphia();
-        //initPlayAuthenticate();
+        initPlayAuthenticate();
     }
 
     private void initMorphia() {
@@ -59,59 +59,59 @@ public class Global extends GlobalSettings {
     }
 
     private void initPlayAuthenticate() {
-		PlayAuthenticate.setResolver(new Resolver() {
+        Logger.info("initPlayAuthenticate");
+        PlayAuthenticate.setResolver(new Resolver() {
+            @Override
+            public Call login() {
+                // Your login page
+                return routes.Application.login();
+            }
 
-			@Override
-			public Call login() {
-				// Your login page
-				return routes.Application.login();
-			}
+            @Override
+            public Call afterAuth() {
+                // The user will be redirected to this page after authentication
+                // if no original URL was saved
+                return routes.Application.index();
+            }
 
-			@Override
-			public Call afterAuth() {
-				// The user will be redirected to this page after authentication
-				// if no original URL was saved
-				return routes.Application.index();
-			}
+            @Override
+            public Call afterLogout() {
+                return routes.Application.index();
+            }
 
-			@Override
-			public Call afterLogout() {
-				return routes.Application.index();
-			}
+            @Override
+            public Call auth(final String provider) {
+                // You can provide your own authentication implementation,
+                // however the default should be sufficient for most cases
+                return com.feth.play.module.pa.controllers.routes.Authenticate
+                        .authenticate(provider);
+            }
 
-			@Override
-			public Call auth(final String provider) {
-				// You can provide your own authentication implementation,
-				// however the default should be sufficient for most cases
-				return com.feth.play.module.pa.controllers.routes.Authenticate
-						.authenticate(provider);
-			}
+            @Override
+            public Call askMerge() {
+                return routes.Account.askMerge();
+            }
 
-			@Override
-			public Call askMerge() {
-				return routes.Account.askMerge();
-			}
+            @Override
+            public Call askLink() {
+                return routes.Account.askLink();
+            }
 
-			@Override
-			public Call askLink() {
-				return routes.Account.askLink();
-			}
+            @Override
+            public Call onException(final AuthException e) {
+                if (e instanceof AccessDeniedException) {
+                    return routes.Signup
+                            .oAuthDenied(((AccessDeniedException) e)
+                            .getProviderKey());
+                }
 
-			@Override
-			public Call onException(final AuthException e) {
-				if (e instanceof AccessDeniedException) {
-					return routes.Signup
-							.oAuthDenied(((AccessDeniedException) e)
-									.getProviderKey());
-				}
+                // more custom problem handling here...
+                return super.onException(e);
+            }
+        });
 
-				// more custom problem handling here...
-				return super.onException(e);
-			}
-		});
-
-		//FIXME DATA!!! initialData();
-
+        //FIXME DATA!!! initialData();
+        Logger.info("PlayAuthenticate.resolver="+PlayAuthenticate.getResolver());
     }
 
     private void initialData() {
