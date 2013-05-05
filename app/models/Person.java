397db232f64f48346a839d53685e5a3c6f93f875
Eric Link <eric@ericmlink.com>
2013-05-05 13:38:35 -0500

    ignore missing refs

--- app/models/Person.java
+++ app/models/Person.java
@@ -10,35 +10,35 @@ import java.util.Date;
 import static models.BaseModel.ds;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
-@Entity(noClassnameStored=true)
+@Entity(noClassnameStored = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Person extends BaseModel {
 
-    @Required
-    public Name name;
-    public Date dob;
-    @Required
-    public ContactInfo contactInfo;
-    public Date agreedToTermsAndConditions;
-    public Date agreedToPrivacyPolicy;
-    public List<String> roles = new ArrayList<String>();
-    public List<String> groups = new ArrayList<String>();
-	@Reference
+	@Required
+	public Name name;
+	public Date dob;
+	@Required
+	public ContactInfo contactInfo;
+	public Date agreedToTermsAndConditions;
+	public Date agreedToPrivacyPolicy;
+	public List<String> roles = new ArrayList<String>();
+	public List<String> groups = new ArrayList<String>();
+	@Reference(ignoreMissing = true)
 	public Group group;
-	
-    public static Person find(String id) {
-        return ds.find(Person.class).field("_id").equal(new ObjectId(id)).get();
-    }
 
-    public static List<Person> all() {
-        return allItems(Person.class);
-    }
+	public static Person find(String id) {
+		return ds.find(Person.class).field("_id").equal(new ObjectId(id)).get();
+	}
 
-    public static void save(Person item) {
-        saveItem(item);
-    }
+	public static List<Person> all() {
+		return allItems(Person.class);
+	}
 
-    public static void delete(String idToDelete) {
-        deleteItem(Person.class, find(idToDelete));
-    }
+	public static void save(Person item) {
+		saveItem(item);
+	}
+
+	public static void delete(String idToDelete) {
+		deleteItem(Person.class, find(idToDelete));
+	}
 }
